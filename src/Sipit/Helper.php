606f2407f1d370bbebde3532a83788689cf51a0f
Danny Carrillo <odannycx@gmail.com>
2016-06-08 09:13:29 -0700

    Fixes issues with styleci, only helper for now

--- src/Sipit/Helper.php
+++ src/Sipit/Helper.php
@@ -10,13 +10,16 @@
  */
 
 namespace Sipit;
+
 use Exception;
 
-/* Class for some helper methods 
+/* Class for some helper methods
  * for the Sipit application
  */
-class Helper {
-	public static function verifyIpFormat($ip) {
+class Helper
+{
+	public static function verifyIpFormat($ip)
+	{
 		if ($ip == '') {
 			throw new Exception('IP Address not provided.');
 		}
@@ -24,12 +27,14 @@ class Helper {
 			throw new Exception('IP address not in the correct format.');
 		}
 	}
-	public static function verifyPortFormat($port) {
+	public static function verifyPortFormat($port)
+	{
 		if ($port != '' && ((int)$port < 1024 || (int)$port > 65535)) {
 			throw new Exception('Port number not valid. Must be between 1024 and 65535.');
 		}
 	}
-	public static function verifyIpAndPort($ip,$port) {
+	public static function verifyIpAndPort($ip,$port)
+	{
 		self::verifyIpFormat($ip);
 		self::verifyPortFormat($port);
 	}
