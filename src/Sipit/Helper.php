9de30f40e2354362e4d316e1be0e4319d309c035
Danny Carrillo <odannycx@gmail.com>
2016-06-08 09:37:22 -0700

    Convert tabs to spaces

--- src/Sipit/Helper.php
+++ src/Sipit/Helper.php
@@ -18,39 +18,39 @@ use Exception;
  */
 class Helper
 {
-	/**
-	 * Takes in an IP address and makes sure its formatted correctly.
-	 *
-	 * @throws Exception
-	 */
-	public static function verifyIpFormat($ip)
-	{
-		if ($ip == '') {
-			throw new Exception('IP Address not provided.');
-		}
-		if (!preg_match('/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/', $ip)) {
-			throw new Exception('IP address not in the correct format.');
-		}
-	}
+    /**
+     * Takes in an IP address and makes sure its formatted correctly.
+     *
+     * @throws Exception
+     */
+    public static function verifyIpFormat($ip)
+    {
+        if ($ip == '') {
+            throw new Exception('IP Address not provided.');
+        }
+        if (!preg_match('/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/', $ip)) {
+            throw new Exception('IP address not in the correct format.');
+        }
+    }
 
-	/**
-	 * Takes in a Port number and makes sure its formatted correctly.
-	 *
-	 * @throws Exception
-	 */
-	public static function verifyPortFormat($port)
-	{
-		if ($port != '' && ((int) $port < 1024 || (int) $port > 65535)) {
-			throw new Exception('Port number not valid. Must be between 1024 and 65535.');
-		}
-	}
+    /**
+     * Takes in a Port number and makes sure its formatted correctly.
+     *
+     * @throws Exception
+     */
+    public static function verifyPortFormat($port)
+    {
+        if ($port != '' && ((int) $port < 1024 || (int) $port > 65535)) {
+            throw new Exception('Port number not valid. Must be between 1024 and 65535.');
+        }
+    }
 
-	/**
-	 * Simple helper to run two methods, verify IP and Port.
-	 */
-	public static function verifyIpAndPort($ip,$port)
-	{
-		self::verifyIpFormat($ip);
-		self::verifyPortFormat($port);
-	}
+    /**
+     * Simple helper to run two methods, verify IP and Port.
+     */
+    public static function verifyIpAndPort($ip,$port)
+    {
+        self::verifyIpFormat($ip);
+        self::verifyPortFormat($port);
+    }
 }
